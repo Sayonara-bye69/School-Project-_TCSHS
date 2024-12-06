b5e0c70e39f10ad9c4ee057466017cb3651cf356
Sayonara-bye69 <72963637+Sayonara-bye69@users.noreply.github.com>
2024-12-06 13:58:48 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "HIII :>",
+    introTitle: 'Hi Trixie ;>>',
+    introDesc: `It's my first time making this (kahapon lang ni actually), I hope you like it:>`,
+    btnIntro: '^^HiHi^^',
+    title: 'I heard you like Nothing by Bruno Major, so here is a little something :>',
+    desc: 'mwehehehee',
+    btnYes: 'CLICK THIS!',
+    btnNo: 'No way :3',
+    question: 'Do you enjoy our moments together? (ANSWER!!)',
+    btnReply: 'Send to you <3',
+    reply: 'YES! I REALLY DO! Wait... this is not what I typed :<< Oh well, I actually mean it when I said YES!',
+    mess: 'I KNEWWW ITTT! mwaaa hehehe',
+    messDesc: 'Click "Direct me to my top 2 song hehehe',
+    btnAccept: 'Direct me to my top 2 song hehehe',
+    messLink: 'https://www.youtube.com/watch?v=Is4lgBBGPDM' //link to your messages. e.g., https://www.facebook.com/messages/t/100014188333536
+}
