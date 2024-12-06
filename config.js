29d0d0960e4546969a9f214f94a263fa55235016
Sayonara-bye69 <72963637+Sayonara-bye69@users.noreply.github.com>
2024-12-06 16:43:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Send to you <3',
     reply: 'back then bitaw, I always anticipated your presence  sa Roblox because truth be told, I really wanted to play with you HAHAHHAAH! I usually checked if you were online and if you were, maghilom hilom rako kay torpe man si me. But now that I can invite you freely to any game, I am really really glad our relationship upgraded to the next Level (murag Level 3 na ni eyyyy). For the days that I couldnt catch up with you or the days where I fall short because I didnt meet your expectations. But yes, truth be told, I really enjoy our moments together hehehe',
     mess: 'I KNEWWW ITTT! mwaaa hehehe',
-    messDesc: 'Click the button below for a surpise (or is it? mwhehehehe)',
+    messDesc: 'Click the button below for a surprise (or is it? mwhehehehe)',
     btnAccept: 'SUPRISEEEEE!',
     messLink: 'https://sayonara-bye69.github.io/vids-/' 
 }
