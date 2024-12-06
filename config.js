44642f33347d8798d6f6bd2b72111f24f5ee9de7
Sayonara-bye69 <72963637+Sayonara-bye69@users.noreply.github.com>
2024-12-06 17:06:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnYes: 'CLICK THIS!',
     btnNo: 'No way :3',
     question: 'Do you enjoy our moments together? (oh, this is going to be long...)',
-    btnReply: 'Send to you <3',
+    btnReply: 'Send this to gill <3',
     reply: 'back then bitaw, I always anticipated your presence  sa Roblox because truth be told, I really wanted to play with you HAHAHHAAH! I usually checked if you were online and if you were, maghilom hilom rako kay torpe man si me. But now that I can invite you freely to any game, I am really really glad our relationship upgraded to the next Level (murag Level 3 na ni eyyyy). For the days that I couldnt catch up with you or the days where I fall short because I didnt meet your expectations. But yes, truth be told, I really enjoy our moments together hehehe',
     mess: 'I KNEWWW ITTT! mwaaa hehehe',
     messDesc: 'Click the button below for a surpriseee!! (or is it? mwhehehehe)',
